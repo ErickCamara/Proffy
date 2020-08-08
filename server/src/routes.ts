37daae88d680d82fc3418d6ba3abe3c7fff12aa0
Erick Camara <erickcamara3@gmail.com>
2020-08-08 00:24:29 -0300

    Initial commit

---
+++ server/src/routes.ts
@@ -0,0 +1,16 @@
+import express, { response } from 'express';
+import ClassController from './controllers/Classesontroller';
+import ConnectionsController from './controllers/ConnectionsController';
+
+
+const routes = express.Router();
+const classesControllers = new ClassController();
+const connectionsController = new ConnectionsController();
+
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
+
+export default routes;
